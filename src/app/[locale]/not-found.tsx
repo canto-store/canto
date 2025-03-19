6a78808ec988b4846bf6c2acbcb5ed7efa7da6f7
soubky0 <omar.soubky@proton.me>
2025-03-19 09:48:01 +0200

    refactor(styles): Update global styles and NotFound component for improved layout and accessibility
    - Rename font variable from --font-body to --font-latin for clarity in global styles.
    - Adjust body height in global styles to ensure full viewport coverage.
    - Enhance NotFound component by adding an alert icon and implementing scroll-to-top functionality on mount for better user experience.

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -2,13 +2,26 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "@/i18n/navigation";
+import { AlertOctagon } from "lucide-react";
+import { useEffect } from "react";
 
 export default function NotFound() {
   const router = useRouter();
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
   return (
     <div className="flex h-[calc(100vh-6.5rem-5rem)] flex-col items-center justify-center text-center">
-      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <AlertOctagon className="text-orange-red h-20 w-20" />
+      <h1 className="mt-4 text-4xl font-bold">404 - Page Not Found</h1>
       <p className="mt-2 text-gray-600">
         Sorry, the page you are looking for does not exist.
       </p>
