725599ba1119f95910f9affe6437d1def1e6bba4
soubky0 <omar.soubky@proton.me>
2025-04-18 22:30:24 +0200

    fix: update BACKEND_URL to use a mock API for development

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const BACKEND_URL = "https://mock.apidog.com/m1/885156-866838-default/api";
 
 const api = axios.create({
   baseURL: BACKEND_URL,
