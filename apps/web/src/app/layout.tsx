742ef6feb3fe898f18950fb84393aff6f4b80901
soubky0 <omar.soubky@proton.me>
2025-05-15 20:47:36 +0300

    chore: update layout component for improved structure and styling

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -24,7 +24,9 @@ export const metadata: Metadata = {
     address: false,
     telephone: false,
   },
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || "https://canto-store.com"),
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_APP_URL || "https://canto-store.com",
+  ),
   openGraph: {
     type: "website",
     siteName: "Canto",
@@ -159,127 +161,5 @@ export default async function RootLayout({
 }: {
   children: ReactNode;
 }) {
-  return (
-    <html suppressHydrationWarning>
-      <head>
-        {/* iOS PWA meta tags */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-touch-fullscreen" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="google-site-verification"
-          content="t62WssBi2mhsGZcIt0vYelveU0BcipnG9O1_cqavTQY"
-        />
-        {/* iOS splash screens - explicit links for better compatibility */}
-        {/* iPad Pro */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-2048-2732.png"
-          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1668-2388.png"
-          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1536-2048.png"
-          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-
-        {/* iPhone 16 Series */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1320-2868.png"
-          media="(device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1206-2622.png"
-          media="(device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1179-2556.png"
-          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-
-        {/* iPhone 15 Series */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1290-2796.png"
-          media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-
-        {/* iPhone 14 Series */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1284-2778.png"
-          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1170-2532.png"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-
-        {/* iPhone X through 13 Series */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1125-2436.png"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1242-2688.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-828-1792.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-
-        {/* Older iPhones */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-750-1334.png"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash-1242-2208.png"
-          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-
-        {/* Apple touch icons */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-touch-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/apple-touch-icon-167x167.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/apple-touch-icon-120x120.png"
-        />
-
-        {/* Add theme-color meta tag */}
-        <meta name="theme-color" content="#ffffff" />
-      </head>
-      {children}
-    </html>
-  );
+  return <html>{children}</html>;
 }
