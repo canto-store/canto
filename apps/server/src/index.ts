c0eaf10922330d0f63ee41a1c674209757c2b2e0
soubky0 <omar.soubky@proton.me>
2025-06-16 07:02:25 +0300

    feat(server): add new allowed origin for CORS configuration
    - Included 'http://164.90.234.81:5173' in the CORS allowed origins list to enable access from the specified IP address.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -18,6 +18,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://localhost:5173',
+      'http://164.90.234.81:5173',
       'https://staging.canto-store.com',
       'http://staging.canto-store.com',
       'https://www.canto-store.com',
