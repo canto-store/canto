533f3db762b4d2801fab5f66f6cf8cdcd301f547
Omar Soubky <omar.soubky@proton.me>
2025-08-02 03:49:05 +0300

    feat: add getProductCount method to DashboardService for product status retrieval

--- apps/server/src/modules/dashboard/dashboard.service.ts
+++ apps/server/src/modules/dashboard/dashboard.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, ProductStatus } from '@prisma/client'
 import { stringifyActivities } from './dashboard.core'
 class DashboardService {
   private readonly prisma = new PrismaClient()
@@ -9,6 +9,25 @@ class DashboardService {
       take: 5,
     })
     return stringifyActivities(activities)
+  }
+
+  async getProductCount() {
+    const products = await this.prisma.product.findMany({
+      where: { OR: [{ status: 'ACTIVE' }, { status: 'PENDING' }] },
+    })
+
+    const activeProductsCount = products.filter(
+      p => p.status === ProductStatus.ACTIVE
+    ).length
+
+    const pendingProductsCount = products.filter(
+      p => p.status === ProductStatus.PENDING
+    ).length
+
+    return {
+      activeProductsCount,
+      pendingProductsCount,
+    }
   }
 }
 
