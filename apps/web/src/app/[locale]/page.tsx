a0f8938485f9859e345653d2c365b81fd7ba1370
Omar Soubky <omar.soubky@proton.me>
2025-08-06 07:46:48 +0300

    feat: refactor Home component; clean up imports and update layout for better readability

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { HomeProducts } from "@/components/home/HomeProducts";
-import { FeaturesBanner } from "@/components/home/FeaturesBanner";
+// import { FeaturesBanner } from "@/components/home/FeaturesBanner";
 import { HomeCategories } from "@/components/home/HomeCategories";
 import { useHomeProducts } from "@/lib/product";
 import { useCartStore, useGetCart } from "@/lib/cart";
 import { useEffect } from "react";
 import Image from "next/image";
+import { RotateCcw } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
   const { data, isLoading } = useHomeProducts();
   const { bestDeals, bestSellers, newArrivals } = data || {};
   const { data: cart, isStale } = useGetCart();
   const { setItems } = useCartStore();
+  const t = useTranslations();
 
   useEffect(() => {
     if (cart && !isStale) {
@@ -30,28 +33,34 @@ export default function Home() {
           className="object-cover"
         />
       </section>
-
       <HomeProducts
         products={bestDeals}
         title="Best Deals"
         isLoading={isLoading}
       />
-
       <HomeCategories />
-
       <HomeProducts
         products={bestSellers}
         title="Best Sellers"
         isLoading={isLoading}
       />
-
       <HomeProducts
         products={newArrivals}
         title="New Arrivals"
         isLoading={isLoading}
       />
-
-      <FeaturesBanner />
+      {/* <FeaturesBanner /> */}
+      <section>
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-center sm:px-6 lg:px-8">
+          <RotateCcw className="mb-3 h-8 w-8 text-gray-500 sm:mb-4" />
+          <h3 className="text-xs font-medium text-gray-500 uppercase sm:text-sm">
+            {t("features.return.title")}
+          </h3>
+          <p className="mt-1 text-xs text-gray-600 sm:mt-2 sm:text-sm">
+            {t("features.return.description")}
+          </p>
+        </div>
+      </section>
     </>
   );
 }
