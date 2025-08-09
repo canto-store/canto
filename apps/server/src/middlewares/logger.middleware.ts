fe2b13fa10373e4d1dec05870973892b6e0321c0
Omar Soubky <omar.soubky@proton.me>
2025-08-09 14:56:09 +0300

    feat: enhance logger middleware to include request timestamp and host information for improved logging

--- apps/server/src/middlewares/logger.middleware.ts
+++ apps/server/src/middlewares/logger.middleware.ts
@@ -13,8 +13,13 @@ const logger = morgan((tokens, req: Request, res: Response) => {
   const authReq = req as AuthRequest
   const userId = authReq.user?.id
   const userInfo = userId ? ` [userId:${userId}]` : ''
+  const host = req.headers.host || ''
+  const hostInfo = host ? ` [host:${host}]` : ''
+  const now = new Date()
+  const formattedTime = now.toLocaleString('en-US')
 
   return [
+    formattedTime,
     tokens.method(req, res),
     tokens.url(req, res),
     getColorStatus(tokens.status(req, res) ?? ''),
@@ -22,6 +27,7 @@ const logger = morgan((tokens, req: Request, res: Response) => {
     tokens['response-time'](req, res),
     'ms',
     userInfo,
+    hostInfo,
   ].join(' ')
 })
 
