7f0c583f8e8d1bec767e98ffddb10a2a6b3ba636
Omar Soubky <omar.soubky@proton.me>
2025-11-11 12:09:21 +0200

    feat: integrate PWA installation component and update type definitions

--- apps/web/src/types/global.d.ts
+++ apps/web/src/types/global.d.ts
@@ -1,5 +1,4 @@
-// src/types/global.d.ts (or wherever you keep your type declarations)
-
+export {};
 interface Navigator {
   /**
    * Indicates if the web application is running in standalone mode (PWA).
@@ -9,4 +8,29 @@ interface Navigator {
   userAgentData?: {
     platform?: string;
   };
+}
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "pwa-install": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        name?: string;
+        icon?: string;
+        description?: string;
+        "disable-install-description"?: string;
+        "disable-screenshots"?: string;
+        "manual-apple"?: string;
+        "manual-chrome"?: string;
+        "install-description"?: string;
+        "disable-chrome"?: string;
+        "disable-edge"?: string;
+        "disable-firefox"?: string;
+        "disable-samsung"?: string;
+        "disable-other"?: string;
+      };
+    }
+  }
 }
