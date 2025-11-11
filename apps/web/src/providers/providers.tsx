c10d9992c8f68a8d1c44e7c36f251d4939bc765c
Mohamed Ashraf <62084282+m7mda4raf99@users.noreply.github.com>
2025-11-11 21:25:25 +0200

    install heroui

--- apps/web/src/providers/providers.tsx
+++ apps/web/src/providers/providers.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import { HeroUIProvider } from "@heroui/react";
 import { BannerProvider } from "./banner/banner-provider";
 import { QueryProvider } from "./query-provider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -10,12 +12,16 @@ interface ProvidersProps {
 }
 
 export function Providers({ children }: ProvidersProps) {
+  const router = useRouter();
+
   return (
-    <QueryProvider>
-      <BannerProvider>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </BannerProvider>
-    </QueryProvider>
+    <HeroUIProvider navigate={router.push}>
+      <QueryProvider>
+        <BannerProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BannerProvider>
+      </QueryProvider>
+    </HeroUIProvider>
   );
 }
