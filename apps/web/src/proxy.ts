7df869fa3c0af8e92ee7600ef75a28bfacf4d2e2
Omar Soubky <omar.soubky@proton.me>
2025-11-19 21:21:24 +0200

    chore: upgrade Next.js to version 16.0.3 and remove experimental reactCompiler option

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -4,7 +4,7 @@ import { routing } from "./i18n/routing";
 
 const intlMiddleware = createMiddleware(routing);
 
-export default function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const cleanPathname = pathname.replace(/^\/(?:ar|en)(?=\/|$)/, "");
